003fd2e11fa82c241dca3c6e69f39cc4247bace4
Анастасия <kvartira-62@yandex.ru>
2022-05-12 17:53:05 +0300

    Не все удалось

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -1,2 +1,24 @@
-package com.company;public class Parser {
+package com.company;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class Parser {
+    ArrayList<String> parse (File file) {
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader( new FileReader(file));
+            String line;
+            while ((line= br.readLine())!= null) {
+                String [] words = line.split(" ");
+                for (String s : words) {
+                    lines.add(s);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
 }
